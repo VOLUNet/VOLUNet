d1eb50684f34e07c52d901035af5046f1fa2c50c
warabimochi <ecccompsenyou@gmail.com>
2025-06-09 14:00:41 +0900

    feat(DB): ボランティアテーブルにカラムを追加する

---
+++ VOLUNet-BE/drizzle/0002_broad_crusher_hogan.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `volunteers` ADD `category` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `volunteers` ADD `num_people` integer NOT NULL;
--- VOLUNet-BE/src/db/schema.ts
+++ VOLUNet-BE/src/db/schema.ts
@@ -3,9 +3,13 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const volunteers = sqliteTable("volunteers", {
   id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
   organizerName: text("name").notNull(),
+  category: text({
+    enum: ["EnvironmentProtection", "Welfare", "CommunityActivity"],
+  }).notNull(),
   volunteerName: text("name").notNull(),
   location: text("location").notNull(),
   eventDate: integer("event_date", { mode: "timestamp" }).notNull(),
+  numPeople: integer("num_people").notNull(),
   description: text("description").notNull(),
   isSharedToStudents: integer("is_shared_to_students", {
     mode: "boolean",
