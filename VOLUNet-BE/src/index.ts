1bc407bcf679486cb5d01a9dde6089a07a894a8b
warabimochi <ecccompsenyou@gmail.com>
2025-06-09 13:42:05 +0900

    feat(volunteer): 登録用APIを作成する

--- VOLUNet-BE/src/index.ts
+++ VOLUNet-BE/src/index.ts
@@ -1,11 +1,46 @@
 import { Hono } from "hono";
+import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
+import { users, volunteers } from "./db/schema";
 
-const app = new Hono();
+type Bindings = {
+  DB: DrizzleD1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/health", (c) => {
   return c.json({
     message: "service up",
   });
+});
+
+interface volunteerPost {
+  volunteerName: string;
+  location: string;
+  eventDate: Date;
+  description: string;
+}
+
+// ボランティア登録用API
+app.post("/volunteer", async (c) => {
+  const db = drizzle(c.env.DB);
+  const body = c.req.json() as unknown as volunteerPost;
+
+  try {
+    const result = await db.insert(volunteers).values({
+      // TODO: 認可ロジックがいる
+      organizerName: "",
+      volunteerName: body.volunteerName,
+      location: body.location,
+      eventDate: body.eventDate,
+      description: body.description,
+    });
+
+    return c.json({ message: "ボランティアデータを正常に登録しました。" });
+  } catch (e) {
+    c.status(500);
+    return c.json("ボランティアデータを正常に登録出来ませんでした。");
+  }
 });
 
 export default app;
